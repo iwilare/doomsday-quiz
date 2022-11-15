92951f046dc1fb10855085bc428bc48feee380de
iwilare <iwilare@gmail.com>
2022-11-16 01:24:29 +0200

    Add deploy workflow

--- src/main.tsx
+++ src/main.tsx
@@ -7,67 +7,146 @@ import _ from 'lodash'
 const months = (isLeap: boolean) => [31, isLeap ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 const isLeap = (y: number) => y % 4 == 0 && y % 100 != 0 || y % 400 == 0
 const randomDate = (start: Date, end: Date) => new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
-const randomQuizDate = () => randomDate(new Date(1600, 1, 1), new Date(2100, 12, 31))
 
 function getRandomDate() {
-  const y = _.random(1600, 2100, false)
+  const y = _.random(1900, 2100, false)
   const m = _.random(1, 12, false)
-  const d = _.sample(months(isLeap(m)))
-  return new Date(y, m, d);
+  const d = _.random(1, months(isLeap(y))[m - 1], false)
+  return { y: y, m: m, d: d };
 }
 
-const DECADE = [0,5,4,2,1,6,5,3,2,0]
-const YDIGIT = [0,1,2,3,5,6,0,1,3,4]
-const LEAPSX = [2,3,6,7]
-const CENTURY = [2,0,5,3]
-const MONTHS_DIFF = [-2, // jan
-                      1, // feb
-                      0, // mar
-                      3, // apr
-                     -2, // may
-                      1, // jun
-                      3, // jul
-                     -1, // aug
-                      2, // sep
-                     -3, // oct
-                      0, // nov
-                      2  // dec
-                     ]
+const DECADE = [0, 5, 4, 2, 1, 6, 5, 3, 2, 0]
+const YDIGIT = [0, 1, 2, 3, 5, 6, 0, 1, 3, 4]
+const LEAPSX = [2, 3, 6, 7]
+const CENTURY = [2, 0, 5, 3]
+const MONTHS_DIFF = [-2, // jan next year
+  1, // feb next year
+  0, // mar
+  3, // apr
+-2, // may
+  1, // jun
+  3, // jul
+-1, // aug
+  2, // sep
+-3, // oct
+  0, // nov
+  2  // dec
+]
+const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 function myDoomsday(y: number, m: number, d: number) {
-  y += [1,2].includes(m) ? -1 : 0
-  const [c, u, z] = [Math.floor(y / 100), Math.floor(y / 10) % 100, y % 10]
+  y += [1, 2].includes(m) ? -1 : 0
+  const [c, u, z] = [Math.floor(y / 100), Math.floor(y / 10) % 10, y % 10]
   return (
-          CENTURY[c % 4] +
-          DECADE[u] +
-          YDIGIT[z] +
-          (u % 2 != 0 && LEAPSX.includes(z) ? 1 : 0) +
-          MONTHS_DIFF[m - 1] + d
-         ) % 7
+    CENTURY[c % 4] +
+    DECADE[u] +
+    YDIGIT[z] +
+    (u % 2 != 0 && LEAPSX.includes(z) ? 1 : 0) +
+    MONTHS_DIFF[m - 1] + d
+  ) % 7
 }
-const MONTHS = ["january","february","march","april","may","june","july","august","september","october","november","december"]
 
 function App() {
   const [state, setState] = useState({
-    month: _.random(1, 12, false),
-    input: "",
-    won: 0,
+    date: getRandomDate(),
+    won: 0, did: 0,
+    showSolution: false,
+    lastClicked: null as (null | number),
+    isCorrect: null as (null | boolean),
   })
+  function refresh() {
+    setState({
+      ...state,
+      date: getRandomDate(), showSolution: false, isCorrect: null
+    })
+  }
+  const date = new Date(state.date.y, state.date.m - 1, state.date.d)
+  const name = date.toLocaleDateString('default', { month: 'long', year: 'numeric', day: 'numeric' })
+  const correctDay = date.getDay();
+  console.log(state.date, correctDay, state.isCorrect)
   return (
-    <div className="App">
-      <mui.Typography variant="h3">{MONTHS[state.month]}</mui.Typography>
-      <mui.TextField id="outlined-basic" variant="outlined"
-             value={state.input}
-             onChange={e => {
-              (7 + parseInt(e.target.value)) % 7 == (7 + MONTHS_DIFF[state.month]) % 7
-                ? setState({
-                    month: _.random(1, 12, false),
-                    input: "",
-                    won: state.won + 1
-                  }) : setState({...state, input: e.target.value })
-             }}
-             />
-    </div>
+    <mui.Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      style={{ minHeight: '100vh' }}
+    >
+      <mui.DialogTitle variant="h3" color={
+        state.isCorrect === null ? 'default'
+        : state.isCorrect ? 'primary' : 'error'}>
+        {name}
+      </mui.DialogTitle>
+      <mui.DialogTitle variant="h5">{state.won} / {state.did}</mui.DialogTitle>
+      {<div>{[0, 1, 2, 3, 4, 5, 6].map(i =>
+        <mui.Button
+          variant="contained"
+          key={i}
+          color={
+            !state.showSolution ? 'primary'
+            : i == correctDay ? 'success'
+            : i == state.lastClicked ? 'error' :
+            'primary'}
+          onClick={e => {
+            if (state.showSolution) {
+              refresh();
+              return;
+            }
+            let correct = i == correctDay;
+            setState({
+              ...state,
+              won: state.won + (correct ? 1 : 0),
+              did: state.did + 1,
+              isCorrect: correct,
+              lastClicked: i,
+              showSolution: true,
+            })
+          }}>
+          {i}
+        </mui.Button>)}</div>}
+      {
+        (() => {
+          let [y, m, d] = [state.date.y, state.date.m, state.date.d]
+          y += [1, 2].includes(m) ? -1 : 0
+          const [c, u, z] = [Math.floor(y / 100), Math.floor(y / 10) % 10, y % 10]
+
+          const dateComponent = MONTHS_DIFF[m - 1] + (d % 7)
+          const yearComponent = DECADE[u] + YDIGIT[z] + (u % 2 != 0 && LEAPSX.includes(z) ? 1 : 0)
+          const fullYearComponent = CENTURY[c % 4] + yearComponent
+
+          return !state.showSolution ? [] :
+            <div>
+              <mui.Table>
+                <mui.TableRow>
+                  <mui.TableCell>Date:</mui.TableCell>
+                  <mui.TableCell>{MONTHS[m - 1]} {d}</mui.TableCell>
+                  <mui.TableCell>{MONTHS_DIFF[m - 1]} + {d % 7} = {dateComponent}</mui.TableCell>
+                </mui.TableRow>
+
+                <mui.TableRow>
+                  <mui.TableCell>Year:</mui.TableCell>
+                  <mui.TableCell>{u * 10 + z}</mui.TableCell>
+                  <mui.TableCell>{DECADE[u]} + {
+                    YDIGIT[z]} + {(u % 2 != 0 && LEAPSX.includes(z) ? 1 : 0)} = {yearComponent}</mui.TableCell>
+                </mui.TableRow>
+
+                <mui.TableRow>
+                  <mui.TableCell>Full year:</mui.TableCell>
+                  <mui.TableCell>{y}</mui.TableCell>
+                  <mui.TableCell>{CENTURY[c % 4]} + {yearComponent} = {fullYearComponent} </mui.TableCell>
+                </mui.TableRow>
+
+                <mui.TableRow>
+                  <mui.TableCell>Result:</mui.TableCell>
+                  <mui.TableCell></mui.TableCell>
+                  <mui.TableCell>{fullYearComponent} + {dateComponent} = {fullYearComponent + dateComponent} = {(fullYearComponent + dateComponent) % 7}</mui.TableCell>
+                </mui.TableRow>
+
+              </mui.Table>
+              <mui.Button onClick={refresh}>Continue</mui.Button></div>
+        })()
+      }
+    </mui.Grid >
   )
 }
 
