76eb75882a6e463b5682e6fcc2daa23235b92fbe
iwilare <iwilare@gmail.com>
2022-11-26 01:20:02 +0200

    Refactor calculation generation

--- src/main.tsx
+++ src/main.tsx
@@ -11,14 +11,15 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
-import _ from 'lodash'
+import _, { kebabCase } from 'lodash'
 import { styled } from '@mui/material/styles';
+import { color } from '@mui/system';
 
 const months = (isLeap: boolean) => [31, isLeap ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 const isLeap = (y: number) => y % 4 == 0 && y % 100 != 0 || y % 400 == 0
 const randomDate = (start: Date, end: Date) => new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
 
-const DECADE = [0, 5, 4, 2, 1, 6, 5, 3, 2, 0]
+const DECADE = [0, -2, -3, 2, 1, 5, 6, 3, 2, 0]
 const YDIGIT = [0, 1, 2, 3, 5, 6, 0, 1, 3, 4]
 const LEAPSX = [2, 3, 6, 7]
 const CENTURY = [2, 0, 5, 3]
@@ -37,32 +38,16 @@ const MONTHS_DIFF = [
   2  // dec
 ]
 const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-const COLORS = [
-  'purple', // jan next year
-  'purple', // feb next year
-  'lightgrey', // mar
-  'red', // apr
-  'blue', // may
-  'green', // jun
-  'red', // jul
-  'darkgreen', // aug
-  'orange', // sep
-  '#009fff', // oct
-  'lightgrey', // nov
-  'orange'  // dec
-]
 
-function myDoomsday(y: number, m: number, d: number) {
-  y += [1, 2].includes(m) ? -1 : 0
-  const [c, u, z] = [Math.floor(y / 100), Math.floor(y / 10) % 10, y % 10]
-  return (
-    CENTURY[c % 4] +
-    DECADE[u] +
-    YDIGIT[z] +
-    (u % 2 != 0 && LEAPSX.includes(z) ? 1 : 0) +
-    MONTHS_DIFF[m - 1] + d
-  ) % 7
-}
+const colorValue = (x: number) =>
+  [ 'primary',
+    'green',
+    'orange',
+    'red',
+    'blue',
+    'DeepSkyBlue',
+    'violet'
+  ][(x + 7) % 7]
 
 type $ = "day" | "month" | "year" | "wait" | "question" | "answer"
 
@@ -81,10 +66,24 @@ function App() {
     animationTime: 1000,
     showOptions: true,
     showAnimations: false,
-    showColors: true,
     nightMode: useMediaQuery('(prefers-color-scheme: dark)'),
     query: null as (null | Dayjs.Dayjs),
   })
+  const nullColor = state.nightMode ? '#000000' : '#e0e0e0'
+  function Row(_: { k: any, v: any, hideKey?: boolean, hideArrVal?: boolean, swap?: boolean, underlineKey?: boolean }) {
+    return <mui.TableRow>
+      <mui.TableCell align="right">
+        <span style={{border: _.underlineKey ? "1px solid silver" : '', padding: 1.3, borderRadius: 8,
+                      color: _.hideKey ? nullColor
+                           : colorValue(_.v)}}>
+          {_.k}
+        </span>
+      </mui.TableCell>
+      <mui.TableCell align="center" padding="none"
+          sx={{ color: _.hideArrVal ? nullColor : 'primary' }}>{_.swap || false ? '⇵' : '⟶' }</mui.TableCell>
+      <mui.TableCell align="right" sx={{color: _.hideArrVal ? nullColor : 'primary'}}>{_.v}</mui.TableCell>
+    </mui.TableRow>
+  }
   console.log(useMediaQuery('(prefers-color-scheme: dark)'))
   const theme = useMemo(() =>
     createTheme({
@@ -147,10 +146,19 @@ function App() {
   y += [1, 2].includes(m) ? -1 : 0
   const [c, u, z] = [Math.floor(y / 100), Math.floor(y / 10) % 10, y % 10]
 
+  const Computations: [string, number][] = [
+    ['Day',     d % 7],
+    ['Month',   MONTHS_DIFF[m - 1]],
+    ['Century', CENTURY[c % 4]],
+    ['Decade',  DECADE[u]],
+    ['Units',   YDIGIT[z]],
+    ['Leap',    (u % 2 != 0 && LEAPSX.includes(z) ? 1 : 0)],
+  ]
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <mui.Stack sx={{ mt: 4 }} alignItems="center" spacing={1}>
+      <mui.Stack sx={{ mt: 8 }} alignItems="center" spacing={1}>
         <mui.DialogTitle variant="h4" align="center" sx={{ px: 0 }}
           color={ state.isCorrect === null ? 'default'
                 : state.isCorrect ? 'green' : 'error'}>
@@ -162,16 +170,12 @@ function App() {
         </mui.DialogTitle>
         <mui.Box sx={{ px: 1, width: '100%' }}>
           <mui.Box sx={{ display: 'flex', width: '100%', justifyContent: 'center' }}>{[0, 1, 2, 3, 4, 5, 6].map(i =>
-            <mui.Button
-              variant="contained"
-              key={i}
-              sx={{ minWidth: 0, maxWidth: 50, mx: 0.25 }}
-              fullWidth={true}
-              color={
+            <mui.Button variant="contained" key={i} fullWidth={true}
+              sx={{ minWidth: 0, maxWidth: 55, mx: 0.25 }} color={
                 state.$ != 'answer' ? 'primary'
-                  : i == correctDay ? 'success'
-                    : i == state.lastClicked ? 'error' :
-                      'primary'}
+               : i == correctDay ? 'success'
+               : i == state.lastClicked ? 'error'
+               : 'primary'}
               onClick={e => {
                 if (state.$ == 'answer')
                   refresh();
@@ -179,11 +183,11 @@ function App() {
                   const correct = i == correctDay;
                   setState(_ => ({
                     ..._,
+                    $: 'answer',
                     won: _.won + (correct ? 1 : 0),
                     did: _.did + 1,
                     isCorrect: correct,
                     lastClicked: i,
-                    $: 'answer',
                     timeEnd: Date.now(),
                   }))
                 }
@@ -202,76 +206,33 @@ function App() {
           <mui.Stack direction="row" pt={1.5}>
             <mui.Card>
               <mui.Table size="small">
-                {MONTHS_DIFF.map((d, i) => <mui.TableRow sx={{ color: d == 0 ? 'lightgrey' : 'primary' }}>
-                  <mui.TableCell align="right" sx={{
-                    textDecoration: [0, 1].includes(i) ? 'underline' : '',
-                    color: state.showColors ? COLORS[i] : 'primary'
-                  }}>{MONTHS[i % 12]}</mui.TableCell>
-                  <mui.TableCell align="right">{d}</mui.TableCell>
-                </mui.TableRow>)}
+                {MONTHS_DIFF.map((d, i) => <Row underlineKey={[0, 1].includes(i)} hideKey={d == 0} hideArrVal={d == 0} k={MONTHS[i % 12]} v={d}/>)}
               </mui.Table>
             </mui.Card>
             <mui.Card>
               <mui.Table size="small">
-                {DECADE.map((d, i) => <mui.TableRow sx={{ color: i == 0 ? 'lightgrey' : 'primary' }}>
-                  <mui.TableCell><span style={{ color: i % 2 == 1 ? 'red' : 'primary' }}>{i.toString()}</span><span style={{ color: '#eeeeee' }}>0</span></mui.TableCell>
-                  <mui.TableCell padding="none" align="center">{i == 5 ? '⇵'
-                    : i == 6 ? '⇵'
-                      : '⟶'}</mui.TableCell><mui.TableCell>{d}</mui.TableCell>
-                </mui.TableRow>)}
+                {DECADE.map((d, i) => <Row swap={[5, 6].includes(i)} hideKey={[0, 9].includes(i)} hideArrVal={[0, 9].includes(i)} k={<>'{i.toString()}0</>} v={d}/>)}
               </mui.Table>
             </mui.Card>
             <mui.Card>
               <mui.Table size="small">
-                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => <mui.TableRow sx={{ color: [0, 1, 2, 3, 6].includes(i) ? 'lightgrey' : 'primary' }}>
-                  <mui.TableCell align="right" sx={{ color: [2, 3, 6, 7].includes(i) ? 'red' : 'primary' }}>{i}</mui.TableCell><mui.TableCell padding="none">⟶</mui.TableCell>
-                  <mui.TableCell align="right">{(i + Math.floor(i / 4)) % 7}</mui.TableCell>
-                </mui.TableRow>)}
+                {[...Array(10)].map((_, i) => <Row hideKey={[0, 1].includes(i)} hideArrVal={[0, 1, 2, 3, 6].includes(i)} underlineKey={[2,3,6,7].includes(i)} k={i} v={(i + Math.floor(i / 4)) % 7}/>)}
               </mui.Table>
             </mui.Card>
-            {/*<mui.Card>
-              <mui.Table size="small">
-                {[16, 17, 18, 19, 20, 21, 22, 23].map(i => <mui.TableRow sx={{ color: [19, 20].includes(i) ? 'primary' : 'lightgrey' }}>
-                  <mui.TableCell align="right">{i}</mui.TableCell><mui.TableCell padding="none">⟶</mui.TableCell>
-                  <mui.TableCell align="right">{CENTURY[i % 4]}</mui.TableCell>
-                </mui.TableRow>)}
-              </mui.Table>
-            </mui.Card>*/}
           </mui.Stack>
           : <></>}
         {!(state.showTables && state.$ == 'answer') ? [] :
           <div>
-            <mui.Table size="small">
-              <mui.TableRow>
-                <mui.TableCell align="right">Day</mui.TableCell>
-                <mui.TableCell align="right">{d % 7}</mui.TableCell>
-                <mui.TableCell align="right"></mui.TableCell>
-              </mui.TableRow>
-              <mui.TableRow>
-                <mui.TableCell align="right">Month</mui.TableCell>
-                <mui.TableCell align="right"><span style={{ color: [1, 2].includes(m) ? 'blue' : 'primary' }}>{MONTHS_DIFF[m - 1]}</span></mui.TableCell>
-                <mui.TableCell align="right">{(d + MONTHS_DIFF[m - 1]) % 7}</mui.TableCell>
-              </mui.TableRow>
-              <mui.TableRow>
-                <mui.TableCell align="right">Century</mui.TableCell>
-                <mui.TableCell align="right">{CENTURY[c % 4]}</mui.TableCell>
-                <mui.TableCell align="right">{(d + MONTHS_DIFF[m - 1] + CENTURY[c % 4]) % 7}</mui.TableCell>
-              </mui.TableRow>
-              <mui.TableRow>
-                <mui.TableCell align="right">Decade</mui.TableCell>
-                <mui.TableCell align="right">{DECADE[u]}</mui.TableCell>
-                <mui.TableCell align="right">{(d + MONTHS_DIFF[m - 1] + CENTURY[c % 4] + DECADE[u]) % 7}</mui.TableCell>
-              </mui.TableRow>
-              <mui.TableRow>
-                <mui.TableCell align="right">Units</mui.TableCell>
-                <mui.TableCell align="right">{YDIGIT[z]}</mui.TableCell>
-                <mui.TableCell align="right">{(d + MONTHS_DIFF[m - 1] + CENTURY[c % 4] + DECADE[u] + YDIGIT[z]) % 7}</mui.TableCell>
-              </mui.TableRow>
-              <mui.TableRow>
-                <mui.TableCell align="right">Leap</mui.TableCell>
-                <mui.TableCell align="right">{(x => <span style={{ color: x > 0 ? 'red' : 'primary' }}>{x}</span>)(u % 2 != 0 && LEAPSX.includes(z) ? 1 : 0)}</mui.TableCell>
-                <mui.TableCell align="right">{(d + MONTHS_DIFF[m - 1] + CENTURY[c % 4] + DECADE[u] + YDIGIT[z] + (u % 2 != 0 && LEAPSX.includes(z) ? 1 : 0)) % 7}</mui.TableCell>
-              </mui.TableRow>
+            <mui.Table size="small">{
+              Computations.reduce<{tally: number, elements: JSX.Element[]}>(
+                ({tally, elements}, [name, value], i) =>
+                  ({tally: tally + value, elements: [...elements,
+                    <mui.TableRow>
+                      <mui.TableCell align="right">{name}</mui.TableCell>
+                      <mui.TableCell align="right">{value}</mui.TableCell>
+                      <mui.TableCell align="right">{i == 0 ? '' : value == 0 ? '-' : (tally + value) % 7}</mui.TableCell>
+                    </mui.TableRow>]}), {tally: 0, elements: []}).elements
+            }
             </mui.Table>
           </div>
         }
@@ -297,7 +258,7 @@ function App() {
               </LocalizationProvider>
               {state.query !== null ? <mui.Typography variant="h5">{state.query.day()}</mui.Typography> : <></>}
               </mui.Stack>
-              <mui.Stack ml={2} >
+              <mui.Stack>
                 <mui.FormGroup>
                   <mui.FormControlLabel
                     control={<mui.Switch />}
@@ -314,11 +275,6 @@ function App() {
                     checked={state.nightMode}
                     onChange={(_, v) => setState(_ => ({ ..._, nightMode: v }))}
                     label="Night mode" />
-                  <mui.FormControlLabel
-                    control={<mui.Switch />}
-                    checked={!state.showColors}
-                    onChange={(_, v) => setState(_ => ({ ..._, showColors: !v }))}
-                    label="Hide colors" />
                 </mui.FormGroup>
               </mui.Stack>
           </mui.Stack>
